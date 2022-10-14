6331dc13dd77e10963554c9dfe55e1797a2eeaee
SergejShk <sshkurupij11@gmail.com>
2022-10-14 21:03:10 +0300

    changed add selfie, added input file

--- src/components/addSelfie/AddSelfie.tsx
+++ src/components/addSelfie/AddSelfie.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {
   Avatar,
   BtnAddAvatar,
@@ -11,54 +11,66 @@ import {
 import avatar from "../../assets/avatar.png";
 import { AiFillPlusCircle } from "react-icons/ai";
 
-import axios from "axios";
-import Uppy from "@uppy/core";
-import AwsS3 from "@uppy/aws-s3";
-import Webcam from "@uppy/webcam";
-import { DashboardModal } from "@uppy/react";
+// import axios from "axios";
+// import Uppy from "@uppy/core";
+// import AwsS3 from "@uppy/aws-s3";
+// import Webcam from "@uppy/webcam";
+// import { DashboardModal } from "@uppy/react";
 import "@uppy/core/dist/style.css";
 import "@uppy/dashboard/dist/style.css";
 
 const AddSelfie: React.FC = () => {
-  const [isModal, setIsModal] = useState<boolean>(false);
+  // const [isModal, setIsModal] = useState<boolean>(false);
+  const filePicker = useRef<HTMLInputElement>(null);
+  const [selectedFile, setSelectedFile] = useState<FileList | null>(null);
 
-  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
-    setIsModal(true);
+  // const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+  //   setIsModal(true);
+  // };
+
+  // const uppy = new Uppy({
+  //   meta: { type: "avatar" },
+  //   restrictions: { maxNumberOfFiles: 1 },
+  //   autoProceed: true,
+  // });
+  // uppy.use(AwsS3, {
+  //   async getUploadParameters(file: any): Promise<any> {
+  //     try {
+  //       const { data } = await axios.post(`/client/selfie`, {
+  //         extension: file.name.split(".").reverse()[0],
+  //       });
+
+  //       return {
+  //         method: data.data.method,
+  //         url: data.data.url,
+  //         fields: data.data.fields,
+  //         headers: {
+  //           "Content-Type": file.type,
+  //         },
+  //       };
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   },
+  // });
+  // uppy.use(Webcam, {
+  //   modes: ["picture"],
+  //   mirror: true,
+  //   videoConstraints: {
+  //     facingMode: "user",
+  //   },
+  // mobileNativeCamera: isMobile({ tablet: true }),
+  // });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files);
+    setSelectedFile(e.target.files);
   };
 
-  const uppy = new Uppy({
-    meta: { type: "avatar" },
-    restrictions: { maxNumberOfFiles: 1 },
-    autoProceed: true,
-  });
-  uppy.use(AwsS3, {
-    async getUploadParameters(file: any): Promise<any> {
-      try {
-        const { data } = await axios.post(`/client/selfie`, {
-          extension: file.name.split(".").reverse()[0],
-        });
-
-        return {
-          method: data.data.method,
-          url: data.data.url,
-          fields: data.data.fields,
-          headers: {
-            "Content-Type": file.type,
-          },
-        };
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  });
-  uppy.use(Webcam, {
-    modes: ["picture"],
-    mirror: true,
-    videoConstraints: {
-      facingMode: "user",
-    },
-    // mobileNativeCamera: isMobile({ tablet: true }),
-  });
+  const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+    console.log(filePicker.current);
+    filePicker.current?.click();
+  };
 
   return (
     <Wrapper>
@@ -69,13 +81,22 @@ const AddSelfie: React.FC = () => {
         <BtnAddAvatar type="button" onClick={handleClick}>
           <AiFillPlusCircle />
         </BtnAddAvatar>
-        <DashboardModal
+
+        <input
+          className="hidden"
+          type="file"
+          ref={filePicker}
+          accept="image/*,.png,.jpg,.jpeg"
+          onChange={handleChange}
+        />
+
+        {/* <DashboardModal
           uppy={uppy}
           closeModalOnClickOutside
           open={isModal}
           onRequestClose={() => setIsModal(false)}
           plugins={["Webcam"]}
-        />
+        /> */}
       </WrapperAvatar>
     </Wrapper>
   );
